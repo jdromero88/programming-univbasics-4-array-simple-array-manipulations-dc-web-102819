723ab83211232f5c596d8461e14ff0041ff84eb8
José Romero <jdromerov88@gmail.com>
2019-10-20 21:10:09 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+def using_unshift(array, string)
+  array.unshift(string)
+end
+def using_pop(array)
+  new_arreglo = array.pop
+  return new_arreglo
+end
+def pop_with_args(array)
+  new_arreglo = array.pop(2)
+  return new_arreglo
+end
+def using_shift(array)
+  array.shift
+end
+def shift_with_args(array)
+    array.shift(2)
+end
+def using_concat(array)
+
+end
